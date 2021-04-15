aacd7f3c934b34394e1f76e4543b90ced1357272
refcell <2238932729@qq.com>
2021-04-15 12:58:13 +0800

    完善用例

--- test3.js
+++ test3.js
@@ -1,5 +1,9 @@
+function f1(a, b, c) { console.log(a + b + c) }
 // 剩余参数语法
 // https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Functions/Rest_parameters
-function f1(a, b, c) { console.log(a + b + c) }
 function f2(...args) { f1(...args) }
 f2(1, 2, 3)
+// https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Functions/arguments
+// https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Function/apply
+function f3() { f1.apply(undefined, arguments)}
+f3(1, 2, 3)
